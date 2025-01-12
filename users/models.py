dfd7575c273d98f5a9a609b91cc9ac9041f3b498
ved_patel <ved@aubergine.co>
2025-01-12 23:15:45 +0530

    separate usermanager

--- users/models.py
+++ users/models.py
@@ -1,34 +1,9 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionsMixin
+  AbstractBaseUser, PermissionsMixin
 )
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        if not username:
-            raise ValueError('Users must have a username')
-        
-        email = self.normalize_email(email)
-        user = self.model(email=email, username = username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, username, password, **extra_fields)
-
+from .managers import CustomUserManager;
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
